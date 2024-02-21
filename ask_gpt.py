b7ab491ff0aa0d6e5d906e16dcf2c834957c8079
wwonnn <dnjsgml0508@naver.com>
2024-02-21 13:58:53 +0900

    add chatbot version

--- ask_gpt.py
+++ ask_gpt.py
@@ -6,7 +6,19 @@ dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 GPT_API_KEY = os.environ['GPT_API_KEY']
 client = OpenAI(api_key=GPT_API_KEY)
+#messages = []
 def create_response(sex, age, request_message):
+    #if(request_message == "대화를 종료합니다"):
+    #   messages.clear()
+    #   return false
+    #
+    # messages.append(
+    #     {
+    #     "role": "user",
+    #     "content": f"I am {sex} and {age} years old, {request_message}"
+    #     }
+    #     )
+    
     response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
@@ -15,10 +27,19 @@ def create_response(sex, age, request_message):
         "content": f"I am {sex} and {age} years old, {request_message}"
         }
     ],
+    #messages = messages,
     temperature=1,
     max_tokens=256,
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0
     )
+    
+    # messages.append(
+    # {
+    # "role": "assistant",
+    # "content": f"{response.choices[0].message.content}"
+    # }
+    # )
+    #return messages[-1].content
     return response.choices[0].message.content
